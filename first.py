dc33f9dc9d51faf81311643bd0a9d8234a4715c4
milnavig <46269335+milnavig@users.noreply.github.com>
2018-12-31 00:37:11 +0200

    Update first.py

--- first.py
+++ first.py
@@ -5,10 +5,10 @@ print(3 * 4)
 print(25 / 5)
 print(-5 * 5)
 print(-(-5 * 5))
-print(--5)
+print(--5) # выведет 5
 print(+5)
 print(100/3)
 print(5 + 5.0)
-print(5**2)
-print(20//6)
-print(20%6)
+print(5**2) # степень
+print(20//6) # выведет 3
+print(20%6) # выведет 2
