e536ed2bfb2b3c4706442e32c6574fad4230e2b5
milnavig <46269335+milnavig@users.noreply.github.com>
2019-01-01 23:59:34 +0200

    Update first.py

--- first.py
+++ first.py
@@ -12,3 +12,25 @@ print(5 + 5.0)
 print(5**2) # степень
 print(20//6) # выведет 3
 print(20%6) # выведет 2
+
+print('Hi!!')
+print("Hi!!")
+print("Hi!! \"Alex") # экранирование
+print("Hi!! \nAlex")
+print('''Hi!! 
+Alex''') # многострочное сообщение
+input('What is your name?: ')
+print(input('What is your name?: '))
+test = 10
+print(test)
+a = input('Введите первое число: ')
+b = input('Введите второе число: ')
+print(a + b) # если а и b например 2 выведет 22
+print(int(a) + int(b))
+print('Первая строка' + 'Вторая строка') # конкатенация строк
+print("2" + "2") # 22
+print("Привет" * 4) # 4 раза выводим Привет
+name = input('Введите Ваше имя: ')
+count = input('Сколько раз умножить: ')
+print( name * int(count))
+print(4 * "Привет") # 4 раза выводим Привет
